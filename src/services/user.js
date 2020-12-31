219d9f6074a9f60c29234d962b31cde801c3f90d
Ryan White <ryanjwhite10@gmail.com>
2020-12-31 10:07:05 -0800

    Parse fetched posts

--- src/services/user.js
+++ src/services/user.js
@@ -1,3 +1,6 @@
+import { sanitize } from "dompurify";
+import marked from "marked";
+
 export const getUserData = async (token, tokenType = "bearer") => {
   const endpoint = "https://oauth.reddit.com/api/v1/me?raw_json=1";
   const response = await fetch(endpoint, {
@@ -45,9 +48,36 @@ export const getAllUserSavedPosts = async (user) => {
       after: currentPostId,
       count: savedPosts.length,
     });
-    savedPosts.push(...response.data.children);
+    const posts = parseSavedPosts(response.data.children);
+    savedPosts.push(...posts);
     currentPostId = response.data.after;
   } while (currentPostId);
 
   return savedPosts;
+};
+
+const parseSavedPosts = (posts) => {
+  return posts.map((post) => {
+    const type = post.kind === "t1" ? "comment" : "link";
+    const title = type === "link" ? post.data.title : post.data.link_title;
+    const content =
+      type === "comment" && typeof post.data.body !== "undefined"
+        ? post.data.body
+        : type === "link" && typeof post.data.selftext !== "undefined"
+        ? post.data.selftext
+        : null;
+
+    return {
+      type,
+      title: sanitize(marked.parseInline(title), {
+        USE_PROFILES: { html: true },
+      }),
+      content: sanitize(marked(content), { USE_PROFILES: { html: true } }),
+      id: post.data.name,
+      subreddit: post.data.subreddit_name_prefixed,
+      permalink: post.data.permalink,
+      url: type === "link" ? post.data.url : null,
+      thumbnail: type === "link" ? post.data.thumbnail : null,
+    };
+  });
 };
