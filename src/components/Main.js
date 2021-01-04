e2ec5cc91aad5e247427b05e1e0b658c4129e808
Ryan White <ryanjwhite10@gmail.com>
2021-01-04 10:39:55 -0800

    Add basic nav w/ search bar

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { authenticateUser } from "../services/auth";
 import { getAllUserSavedPosts } from "../services/user";
 import storage from "../utils/storage";
+import Nav from "./Nav";
 import Card from "./Card";
 
 const POST_INTERVAL = 10;
@@ -42,23 +43,30 @@ const Main = ({ user, posts, setPosts }) => {
     return <div>loading...</div>;
   }
 
-  return displayedPosts.length ? (
-    <div className="md:container md:max-w-screen-xl md:mx-auto p-4">
-      <h1 className="text-4xl font-extrabold">Saved Posts</h1>
-      {displayedPosts.map((post) => (
-        <Card post={post} />
-      ))}
-      {displayedPosts.length < posts.length ? (
-        <button
-          className="block py-2 px-4 bg-orange hover:bg-orange-light text-white font-bold rounded mx-auto"
-          onClick={loadMorePosts}
-        >
-          Load More
-        </button>
-      ) : null}
-    </div>
-  ) : (
-    <p>No saved posts!</p>
+  return (
+    <>
+      <Nav user={user} />
+      <div className="md:container md:max-w-screen-xl md:mx-auto p-4">
+        {displayedPosts.length ? (
+          <>
+            <h1 className="text-4xl font-extrabold">Saved Posts</h1>
+            {displayedPosts.map((post) => (
+              <Card key={post.id} post={post} />
+            ))}
+            {displayedPosts.length < posts.length ? (
+              <button
+                className="block py-2 px-4 bg-orange hover:bg-orange-light text-white font-bold rounded mx-auto"
+                onClick={loadMorePosts}
+              >
+                Load More
+              </button>
+            ) : null}
+          </>
+        ) : (
+          <p>No saved posts!</p>
+        )}
+      </div>
+    </>
   );
 };
 
---
+++ src/components/Nav.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+const Nav = ({ user }) => {
+  const [search, setSearch] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(search);
+  };
+
+  return (
+    <div className="py-2 bg-gray-100">
+      <div className="grid sm:grid-cols-2 grid-gap-4 justify-center sm:justify-start md:container md:max-w-screen-xl md:mx-auto px-4">
+        <div className="flex items-center text-sm font-semibold">{`u/${user.username}`}</div>
+        <form
+          className="sm:justify-self-end pt-2 sm:pt-0"
+          onSubmit={handleSubmit}
+        >
+          <input
+            type="text"
+            className="py-1 px-1.5 mr-2 border border-gray-300 rounded"
+            value={search}
+            onChange={({ target }) => setSearch(target.value)}
+          />
+          <button
+            className="py-1 px-4 bg-orange border border-orange hover:bg-orange-light text-white font-bold rounded"
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
