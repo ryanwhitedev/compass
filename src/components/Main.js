919ee8218e466f508b8e56cbe7a8b3ba72d10c93
Ryan White <ryanjwhite10@gmail.com>
2021-01-04 09:43:12 -0800

    Limit initially rendered posts and conditionally load more

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { authenticateUser } from "../services/auth";
 import { getAllUserSavedPosts } from "../services/user";
 import storage from "../utils/storage";
 import Card from "./Card";
 
+const POST_INTERVAL = 10;
+
 const Main = ({ user, posts, setPosts }) => {
+  const [displayedPosts, setDisplayedPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const loadMorePosts = () => {
+    const numPosts = displayedPosts.length + POST_INTERVAL;
+    const postsToDisplay = posts.slice(0, numPosts);
+    setDisplayedPosts(postsToDisplay);
+  };
+
   useEffect(() => {
     const getAndSetPosts = async () => {
       const posts = await getAllUserSavedPosts(user);
@@ -12,22 +23,39 @@ const Main = ({ user, posts, setPosts }) => {
 
       setPosts(posts);
       storage.savePosts(posts);
+
+      setLoading(false);
+      setDisplayedPosts(posts.slice(0, POST_INTERVAL));
     };
     if (user && !posts.length) {
       getAndSetPosts();
+      setLoading(true);
+    } else if (posts.length) {
+      setDisplayedPosts(posts.slice(0, POST_INTERVAL));
     }
   }, [user, posts, setPosts]);
 
   if (!user || !user.isAuthenticated) {
     return <button onClick={() => authenticateUser()}>auth</button>;
   }
+  if (loading) {
+    return <div>loading...</div>;
+  }
 
-  return posts.length ? (
+  return displayedPosts.length ? (
     <div className="md:container md:max-w-screen-xl md:mx-auto p-4">
       <h1 className="text-4xl font-extrabold">Saved Posts</h1>
-      {posts.map((post) => (
+      {displayedPosts.map((post) => (
         <Card post={post} />
       ))}
+      {displayedPosts.length < posts.length ? (
+        <button
+          className="block py-2 px-4 bg-orange hover:bg-orange-light text-white font-bold rounded mx-auto"
+          onClick={loadMorePosts}
+        >
+          Load More
+        </button>
+      ) : null}
     </div>
   ) : (
     <p>No saved posts!</p>
