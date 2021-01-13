4e53532f21aecce66889f7477316fa7089b2f666
Ryan White <ryanjwhite10@gmail.com>
2021-01-13 14:47:29 -0800

    Style post markdown

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,35 @@
 import React from "react";
 
 const Card = ({ post }) => (
-  <div className="flex flex-wrap bg-white sm:flex-nowrap my-4 p-4 border-solid border border-gray-100 rounded-md shadow">
+  <div className="flex flex-wrap bg-white text-black sm:flex-nowrap my-4 p-4 border-solid border border-gray-100 rounded-md shadow">
     {post.thumbnail && post.thumbnail.includes("https") ? (
       <div className="flex-fw sm:flex-initial sm:flex-shrink-0 sm:mr-4 mb-4 sm:mb-0">
         <img className="mx-auto" src={post.thumbnail} alt="preview thumbnail" />
       </div>
     ) : null}
-    <div className="flex flex-col flex-fw sm:flex-auto sm:flex-grow">
+    <div className="flex flex-col flex-fw sm:flex-auto sm:flex-grow min-w-0">
       <h2
         className={`${
           post.type === "link"
             ? "text-xl sm:text-3xl font-semibold"
             : "text-base sm:text-lg font-bold uppercase"
         }`}
-        dangerouslySetInnerHTML={{ __html: post.title }}
-      ></h2>
+      >
+        {post.type === "comment" ? (
+          <span className="block text-sm leading-none text-orange-light">
+            Comment on:
+          </span>
+        ) : null}
+        <a
+          href={`https://www.reddit.com${post.permalink}`}
+          className="hover:text-orange-light"
+          dangerouslySetInnerHTML={{ __html: post.title }}
+        ></a>
+      </h2>
       <span className="block text-sm font-bold mb-2">{`r/${post.subreddit}`}</span>
       {post.content ? (
         <div
-          className="markdown-wrapper mb-4"
+          className="markdown-wrapper text-sm sm:text-base mb-4"
           dangerouslySetInnerHTML={{ __html: post.content }}
         ></div>
       ) : null}
--- src/services/user.js
+++ src/services/user.js
@@ -2,18 +2,6 @@ import { sanitize } from "dompurify";
 import marked from "marked";
 import storage from "../utils/storage";
 
-// Override default parser output
-const renderer = {
-  code(code, infostring, escaped) {
-    return `<pre><code>${code}</code></pre>`;
-  },
-  codespan(code) {
-    const codeStr = code.replace(/&amp;/g, "&");
-    return `<code>${codeStr}</code>`;
-  },
-};
-marked.use({ renderer });
-
 export const getUserData = async (token, tokenType = "bearer") => {
   const endpoint = "https://oauth.reddit.com/api/v1/me?raw_json=1";
   const response = await fetch(endpoint, {
@@ -76,7 +64,6 @@ export const getNewPosts = async (user) => {
 
   do {
     const response = await getSavedPosts({ user, before: beforePostId });
-    console.log(response);
     const newPosts = parseSavedPosts(response.data.children);
     allPosts = newPosts.concat(allPosts);
     beforePostId = allPosts[0].id;
@@ -87,6 +74,18 @@ export const getNewPosts = async (user) => {
 };
 
 const parseSavedPosts = (posts) => {
+  // Override default parser output
+  const renderer = {
+    code(code) {
+      return `<pre><code>${code}</code></pre>`;
+    },
+    codespan(code) {
+      const codeStr = code.replace(/&amp;/g, "&");
+      return `<code>${codeStr}</code>`;
+    },
+  };
+  marked.use({ renderer });
+
   return posts.map((post) => {
     const type = post.kind === "t1" ? "comment" : "link";
     const title = type === "link" ? post.data.title : post.data.link_title;
