d23320f91d28171cd20f2ee90ec10c7707edaf05
Ryan White <ryanjwhite10@gmail.com>
2021-01-20 09:59:04 -0800

    Update config for prod

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,12 @@
+const isDev =
+  window.location.href.includes("localhost") ||
+  window.location.href.includes("127.0.0.1");
+
 const DEV_CLIENT_ID = "5cdwTjQLHGSubA";
 const DEV_REDIRECT_URI = "http://localhost:3000/signin";
+
+const PROD_CLIENT_ID = "FZTcrrxOiok8uA";
+const PROD_REDIRECT_URI = "https://compass.ryanwhite.dev/signin";
 
 const BASE_AUTH_URL = "https://www.reddit.com/api/v1/authorize";
 const AUTH_TYPE = "token";
@@ -8,8 +15,8 @@ const AUTH_SCOPE = ["identity", "history", "save"];
 
 export const authConfig = {
   baseUrl: BASE_AUTH_URL,
-  clientId: DEV_CLIENT_ID,
-  redirectUri: DEV_REDIRECT_URI,
+  clientId: isDev ? DEV_CLIENT_ID : PROD_CLIENT_ID,
+  redirectUri: isDev ? DEV_REDIRECT_URI : PROD_REDIRECT_URI,
   type: AUTH_TYPE,
   duration: AUTH_DURATION,
   scope: AUTH_SCOPE.join(","),
